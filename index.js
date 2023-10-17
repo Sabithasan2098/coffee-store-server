64c6b56bf5bf1ae62b893d7f7a173c36e14dffc0
Sabithasan2098 <sabithasan2098@gmail.com>
2023-10-17 18:23:32 +0600

    successfully done server

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const coffeeColection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db("coffeeDB").collection("user");
     // -----------__________--------------
     // mongodb te data set korar system
     app.post("/coffee", async (req, res) => {
@@ -41,7 +42,7 @@ async function run() {
     });
 
     // ---------------------------
-    // mongodb theke data localhost api te nia aser system
+    // mongodb theke coffee data localhost api te nia aser system
 
     app.get("/coffee", async (req, res) => {
       const cursor = coffeeColection.find();
@@ -92,6 +93,45 @@ async function run() {
     });
 
     // -----------------------------
+    // user er data database e save korar system
+
+    app.post("/user", async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // -------------------------------
+    // monogodb theke user er data ui te dekhanor system
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // ----------------------------------------
+    // monogodb theke user ke delete korar system
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    // --------------------------------------
+    // mongodb er user update
+
+    app.patch("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updatedDoc = {
+        $set: {
+          lastLoggedAt: user.lastLoggedAt,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
